3b7268ef6685f387c53c3253cd4e052146641d1e
amishari <81028814+amishari@users.noreply.github.com>
2022-12-05 07:27:06 +0330

    Update Price.js

--- src/components/Price.js
+++ src/components/Price.js
@@ -4,7 +4,7 @@ export default function Price(props) {
 	let price = props.price.price;
 	return (
 		<div className="right">
-			<span className="price">{price} تومان</span>
+			<span className="price">{price} ریال</span>
 		</div>
 	);
 }
