0540b73eb1698544919cc6726129c5e4f246753b
amishari <81028814+amishari@users.noreply.github.com>
2022-12-07 08:01:43 +0330

    List updated with a better logic to display
    displaying logic is modified. more professional.

--- src/components/List.js
+++ src/components/List.js
@@ -1,13 +1,35 @@
 import React from "react";
 import ShoppingCard from "./Shopping-card";
 const List = (props) => {
-  let searchedTerm = <p>Nothing found!</p>;
-  if (props.list.length > 0) {
-    searchedTerm = props.list.map((item) => {
-      return <ShoppingCard card={item} />;
-    });
+  if  (props.list.length === 0){
+    return <h2> Nothing Foung! Try again. </h2>
   }
-  return <div>{searchedTerm}</div>;
+
+  return(
+    <div>
+        {props.list.map((item) => {
+          return  <ShoppingCard card={item} />;
+          })};
+        
+    </div>    
+   );
+   
+ 
 };
 
 export default List;
+
+
+// import React from "react";
+// import ShoppingCard from "./Shopping-card";
+// const List = (props) => {
+//   let searchedTerm = <p>Nothing found!</p>;
+//   if (props.list.length > 0) {
+//     searchedTerm = props.list.map((item) => {
+//       return <ShoppingCard card={item} />;
+//     });
+//   }
+//   return <div>{searchedTerm}</div>;
+// };
+
+// export default List;
