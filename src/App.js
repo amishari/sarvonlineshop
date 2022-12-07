4cda4462e68cec06a2fbc66371c72e00e7c3fe12
amishari <81028814+amishari@users.noreply.github.com>
2022-12-07 07:41:59 +0330

    Updated with smart searching

--- src/App.js
+++ src/App.js
@@ -16,13 +16,6 @@ export default function App() {
       product.title.includes(searchTerm) || product.id.includes(searchTerm)
     );
   });
-=======
-	const searchedItems = products.filter((product) => {
-		return (
-			product.title.includes(searchTerm) || product.id.includes(searchTerm)
-		);
-	});
->>>>>>> cd00a1f5dbe241b9ea819be72a15f241829e68d9
 
   return (
     <div className="App">
