4f5021fb095e77ba4e7519ce65f8d28f7ec90fa3
amishari <amishari@yahoo.com>
2022-12-25 22:18:34 +0330

    magnifier added

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,16 +1,36 @@
 import "./style.css";
 import React from "react";
-import ModalImage from "react-modal-image";
+// import ModalImage from "react-modal-image";
+import {
+  // Magnifier,
+  GlassMagnifier,
+  // SideBySideMagnifier,
+  // PictureInPictureMagnifier,
+  // MOUSE_ACTIVATION,
+  // TOUCH_ACTIVATION
+} from "react-image-magnifiers";
 
 export default function Picture(props) {
   let picture = props.picture.picture;
   return (
-    <ModalImage
-      hideDownload="true"
-      imageBackgroundColor="red"
-      small={picture}
-      large={picture}
-      alt={"Code: " + `${props.picture.id}`}
+    // <ModalImage
+    //   hideDownload="true"
+    //   imageBackgroundColor="red"
+    //   small={picture}
+    //   large={picture}
+    //   alt={"Code: " + `${props.picture.id}`}
+    // />
+
+    <GlassMagnifier
+      imageSrc={picture}
+      imageAlt="Example"
+      // largeImageSrc={picture} // Optional
+      allowOverflow="false"
+      magnifierBorderSize="2"
+      magnifierBorderColor="rgba(147, 148, 155, 0.63)"
+      magnifierSize="150%"
+      square="true"
+      magnifierBackgroundColor="rgba(18, 154, 4, 0.13)"
     />
   );
 }
