c276b7c7e4d013ab79b87b92d2feeeae32c83856
amishari <81028814+amishari@users.noreply.github.com>
2022-12-26 08:26:50 +0330

    sideBarMagnifier added

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,36 +1,41 @@
-import "./style.css";
-import React from "react";
+import './style.css';
+import React from 'react';
 // import ModalImage from "react-modal-image";
 import {
-  // Magnifier,
-  GlassMagnifier,
-  // SideBySideMagnifier,
-  // PictureInPictureMagnifier,
-  // MOUSE_ACTIVATION,
-  // TOUCH_ACTIVATION
-} from "react-image-magnifiers";
+	// Magnifier,
+	// GlassMagnifier,
+	SideBySideMagnifier
+	// PictureInPictureMagnifier,
+	// MOUSE_ACTIVATION,
+	// TOUCH_ACTIVATION
+} from 'react-image-magnifiers';
 
 export default function Picture(props) {
-  let picture = props.picture.picture;
-  return (
-    // <ModalImage
-    //   hideDownload="true"
-    //   imageBackgroundColor="red"
-    //   small={picture}
-    //   large={picture}
-    //   alt={"Code: " + `${props.picture.id}`}
-    // />
+	let picture = props.picture.picture;
+	return (
+		// <ModalImage
+		//   hideDownload="true"
+		//   imageBackgroundColor="red"
+		//   small={picture}
+		//   large={picture}
+		//   alt={"Code: " + `${props.picture.id}`}
+		// />
 
-    <GlassMagnifier
-      imageSrc={picture}
-      imageAlt="Example"
-      // largeImageSrc={picture} // Optional
-      allowOverflow="false"
-      magnifierBorderSize="2"
-      magnifierBorderColor="rgba(147, 148, 155, 0.63)"
-      magnifierSize="150%"
-      square="true"
-      magnifierBackgroundColor="rgba(18, 154, 4, 0.13)"
-    />
-  );
+		// <GlassMagnifier
+		//   imageSrc={picture}
+		//   imageAlt="Example"
+		//   // largeImageSrc={picture} // Optional
+		//   allowOverflow="false"
+		//   magnifierBorderSize="2"
+		//   magnifierBorderColor="rgba(147, 148, 155, 0.63)"
+		//   magnifierSize="150%"
+		//   square="true"
+		//   magnifierBackgroundColor="rgba(18, 154, 4, 0.13)"
+		// />
+		<SideBySideMagnifier
+			imageSrc={picture}
+			switchSides
+			overlayBoxColor="green"
+		/>
+	);
 }
